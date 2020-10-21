39ddbe8be60830863b9ec5f2473138cbddd867d9
Benjamin Hofmann <ben@bentop.fritz.box>
2020-10-21 10:26:47 +0200

    htpasswd parameter changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ if [[ -z "${USERNAME}" || -z "${PASS}" ]]; then
     echo "Please supply username and password"
     exit 1
 else
-    htpasswd -cb /etc/apache2/webdav.password $USER $PASS
+    htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASS
     chown root:apache /etc/apache2/webdav.password
     chmod 640 /etc/apache2/webdav.password
     httpd -D FOREGROUND
